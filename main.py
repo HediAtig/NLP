81b18c70654550538a9af7ac1c7c8c2de7459619
Julian Oliveros Forero <je.oliverosf@uniandes.edu.co>
2022-07-09 17:32:26 +0200

    last changes of refactor in the code

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
+# Import libraries
+import plotly.graph_objects as go
+import plotly.express as px
+import streamlit as st
+import pandas as pd
+from regex import regex
 import re
 from numpy import tile
 import nltk
-import pandas as pd
 from nltk import *
 from nltk.sentiment import SentimentIntensityAnalyzer
-from regex import regex
-import streamlit as st
-import plotly.express as px
-import plotly.graph_objects as go
 nltk.download('vader_lexicon')
 
 
+# streamlit create the visual interface.
 uploaded_file = st.sidebar.file_uploader("Upload the chat file:")
 
 if uploaded_file is not None:
 
+    # Function used to
     def ValidDateAndTime(s):
         pattern = '[], []'
         result = re.match(pattern, s)
@@ -35,19 +38,24 @@ if uploaded_file is not None:
 
     # List to keep track of data so it can be used by a Pandas dataframe
     parsedData = []
+
     # Uploading exported chat file
     conversationPath = 'wh_chat.txt'  # chat file
-    fp = uploaded_file
+    filepath = uploaded_file
 
     # Skipping first line of the file because contains
     # information related to something about end-to-end encryption
-    fp.readline()
+    filepath.readline()
     messageBuffer = []
     dateTime, sender = None, None
 
     while True:
 
-        line = fp.readline().decode('utf-8')
+        line = filepath.readline().decode('utf-8')
+
+        print(line)
+
+        
         if not line:
             break
         line = line.strip()
@@ -66,29 +74,30 @@ if uploaded_file is not None:
     df_final_result = pd.DataFrame(
         parsedData, columns=['Date & Time', 'Sender', 'Message'])
 
-    df_final_result = df_final_result.dropna()
-    df_final_result = df_final_result.reset_index(drop=True)
+    df_final_result_without_nulls = df_final_result.dropna()
+    df_final_result_without_nulls = df_final_result_without_nulls.reset_index(
+        drop=True)
 
     sentiments = SentimentIntensityAnalyzer()
 
     # Append the % of positive of the mesages
-    df_final_result["positive"] = [sentiments.polarity_scores(
-        i)["pos"] for i in df_final_result["Message"]]
+    df_final_result_without_nulls["positive"] = [sentiments.polarity_scores(
+        i)["pos"] for i in df_final_result_without_nulls["Message"]]
 
     # Append the % of negative of the mesages
-    df_final_result["negative"] = [sentiments.polarity_scores(
-        i)["neg"] for i in df_final_result["Message"]]
+    df_final_result_without_nulls["negative"] = [sentiments.polarity_scores(
+        i)["neg"] for i in df_final_result_without_nulls["Message"]]
 
     # Append the % of neutral of the mesages
-    df_final_result["neutral"] = [sentiments.polarity_scores(
-        i)["neu"] for i in df_final_result["Message"]]
+    df_final_result_without_nulls["neutral"] = [sentiments.polarity_scores(
+        i)["neu"] for i in df_final_result_without_nulls["Message"]]
 
-    st.dataframe(df_final_result)
+    st.dataframe(df_final_result_without_nulls)
 
-    total_num_messages = len(df_final_result)
+    total_num_messages = len(df_final_result_without_nulls)
 
     # Graph 1 show the number of messages and the sender
-    df_groupby_Sender_number_messages = df_final_result.groupby(
+    df_groupby_Sender_number_messages = df_final_result_without_nulls.groupby(
         ["Sender"], sort=False)["Message"].count().reset_index(drop=False)
 
     fig_number_of_messages_per_sender = px.treemap(df_groupby_Sender_number_messages,
@@ -101,7 +110,7 @@ if uploaded_file is not None:
                     use_container_width=True)
 
     # Graph #2 The global conversation percentage of positive negative and neutral.
-    df_groupby_Sender = df_final_result.sum()
+    df_groupby_Sender = df_final_result_without_nulls.sum()
 
     data = {'values': [df_groupby_Sender["positive"]/total_num_messages, df_groupby_Sender["neutral"]/total_num_messages, df_groupby_Sender["negative"]/total_num_messages],
             'type': ["positive", "neutral", "negative"]}
@@ -117,7 +126,7 @@ if uploaded_file is not None:
     st.plotly_chart(fig_sentiment_global)
 
     # Graph 2 The  conversation percentage of positive negative and neutral.
-    df_groupby_Sender_avg = df_final_result.groupby(
+    df_groupby_Sender_avg = df_final_result_without_nulls.groupby(
         ["Sender"], sort=False).mean().reset_index(drop=False)
 
     fig_percentage_of_message_sentiment = go.Figure(data=[
@@ -144,20 +153,20 @@ if uploaded_file is not None:
     fig_line_graph = go.Figure()
 
     fig_line_graph.add_trace(go.Scatter(
-        x=df_final_result["Date & Time"],
-        y=df_final_result["positive"],
+        x=df_final_result_without_nulls["Date & Time"],
+        y=df_final_result_without_nulls["positive"],
         mode='markers',
         name='sentiment positive'))
 
     fig_line_graph.add_trace(go.Scatter(
-        x=df_final_result["Date & Time"],
-        y=df_final_result["negative"],
+        x=df_final_result_without_nulls["Date & Time"],
+        y=df_final_result_without_nulls["negative"],
         mode='markers',
         name='sentiment negative '))
 
     fig_line_graph.add_trace(go.Scatter(
-        x=df_final_result["Date & Time"],
-        y=df_final_result["neutral"],
+        x=df_final_result_without_nulls["Date & Time"],
+        y=df_final_result_without_nulls["neutral"],
         mode='markers',
         name='sentiment neutral'))
 
